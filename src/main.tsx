f5a9652138588e720e72738c72739b18e629725d
Iban <rodorro@hotmail.com>
2020-02-19 21:16:18 +0100

    Add react router to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {App} from './app';
+import { MembersTableComponent } from './components';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  <HashRouter>
+    <Switch>
+      <Route exact={true} path="/" component={MembersTableComponent} />
+      {/* <Route path="/hotel-collection" component={HotelCollectionScene} /> */}
+    </Switch>
+  </HashRouter>,
   document.getElementById('root')
 );
